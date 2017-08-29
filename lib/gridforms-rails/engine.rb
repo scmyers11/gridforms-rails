1834fd5e41d4f749160f6fde0a573c058b775c19
Stephen Myers <scmyers11@gmail.com>
2017-08-29 17:25:22 -0500

    Include files for js and css

--- lib/gridforms-rails/engine.rb
+++ lib/gridforms-rails/engine.rb
@@ -1,6 +1,9 @@
 module Gridforms
   module Rails
     class Engine < Rails::Engine
+      initializer "gridforms-rails" do |app|
+          app.config.assets.precompile += %w(gridforms.js gridforms.css gridforms.css.map gridforms.sass)
+        en
     end
   end
 end
--- lib/gridforms-rails/version.rb
+++ lib/gridforms-rails/version.rb
@@ -1,5 +1,5 @@
 module Gridforms
   module Rails
-    VERSION = "0.1.0"
+    VERSION = "0.1.1"
   end
 end
